b843193cc45f431a60cea71d1098c0dc04e3ee04
Hamilton Durán <hamy.duran@gmail.com>
2023-05-02 18:57:42 -0600

    Clases Abstractas

--- Cuentas/CuentaCorriente.js
+++ Cuentas/CuentaCorriente.js
@@ -1,5 +1,5 @@
 import { Cuenta } from "./Cuenta.js";
-import { Cliente } from "./Cliente.js";
+import { Cliente } from "../Cliente.js";
 
 
 export class CuentaCorriente extends Cuenta{
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //PARA INICIAR ESTE PROYECTO YA QUE  ES POR MOSULOS , USAR EL COMANDO "npm init" en el powershell, esto crea un .json
 //Para poder usar los modulos de JS colocar el comando "type" = "module" en el archivo json
 import {Cliente} from "./Cliente.js"
-import {CuentaCorriente} from "./CuentaCorriente.js"
-import { CuentaAhorro } from "./CuentaAhorro.js"
-import { Cuenta } from "./Cuenta.js";
-import { CuentaNomina } from "./Cuentanomina.js";
+import {CuentaCorriente} from "./Cuentas/CuentaCorriente.js"
+import { CuentaAhorro } from "./Cuentas/CuentaAhorro.js"
+import { Cuenta } from "./Cuentas/Cuenta.js";
+import { CuentaNomina } from "./Cuentas/CuentaNomina.js";
 
 const cliente = new Cliente ("leonardo", "12345", "24242424");
 const cliente2 = new Cliente ("Maria", "242313256", "34535");
